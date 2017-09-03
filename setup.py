17d82af3f8b97e8256d31394c605764198f985f2
Matteo Hoch <minime@hochweb.com>
2017-09-02 21:37:29 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sdd_api',
+  packages = ['sdd_api'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python API for Sports Data Direct',
+  author = 'Sports Data Direct',
+  author_email = 'peterldowns@gmail.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['sports', 'api', 'sports data direct', 'sdd'], # arbitrary keywords
+  classifiers = [],
+)
